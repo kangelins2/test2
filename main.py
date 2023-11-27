1c0d10288118e06be3db7dfa89ffa5bc3d48fd2a
kangelins2 <kirilova.angelina.kaktysik@gmail.com>
2023-11-27 21:50:38 +0300

    1

---
+++ main.py
@@ -0,0 +1,18 @@
+# def func():
+#     yield 1
+#     yield 2
+#     yield 3
+#
+# s = func()
+# print(next(s))
+
+
+lst = [(1, 2), (3, 4)]
+text = """Всем привет как дела тра та та"""
+pure_text = text.lower().replace(_old:' ', _new:'')
+letters = {
+    letter: pure_text.count(letter)
+    for letter in set(pure_text)
+}
+print(letters)
+
